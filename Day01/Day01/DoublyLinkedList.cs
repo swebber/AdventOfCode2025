f0e98ad1b369e7f53fc17b4a5ec908329cf1a3c5
R. Scott Webber <swebber@thehwpgroup.com>
2025-12-01 19:44:48 -0500

    Day 01

---
+++ Day01/Day01/DoublyLinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day01;
+
+internal class DoublyLinkedListNode<T>
+{
+    public T Value { get; set; }
+    public DoublyLinkedListNode<T> Next { get; set; }
+    public DoublyLinkedListNode<T> Prev { get; set; }
+
+    public DoublyLinkedListNode(T value) => Value = value;
+}
+
+internal class DoublyLinkedList<T>
+{
+    public DoublyLinkedListNode<T> Head { get; private set; }
+    public DoublyLinkedListNode<T> Tail { get; private set; }
+
+    public void AddLast(T value)
+    {
+        var newNode = new DoublyLinkedListNode<T>(value);
+        if (Head == null)
+        {
+            Head = Tail = newNode;
+        }
+        else
+        {
+            Tail.Next = newNode;
+            newNode.Prev = Tail;
+            Tail = newNode;
+        }
+    }
+
+    // Forward traversal
+    public void PrintForward()
+    {
+        var current = Head;
+        while (current != null)
+        {
+            Console.Write(current.Value + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+
+    // Backward traversal
+    public void PrintBackward()
+    {
+        var current = Tail;
+        while (current != null)
+        {
+            Console.Write(current.Value + " ");
+            current = current.Prev;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,56 @@
+﻿using Day01;
+
+var dial = new DoublyLinkedList<int>();
+var current = PopuldateDial(dial);
+
+int zeroCount = 0;
+int zeroTotal = 0;
+
+Console.WriteLine($"Current value: {current.Value}");
+
+var moves = LoadInput("data01.txt");
+foreach (var move in moves)
+{
+    var direction = move[0];
+    var steps = int.Parse(move[1..]);
+
+    for (int i = 0; i < steps; i++)
+    {
+        current = direction == 'L' ? current.Prev : current.Next;
+        if (current.Value == 0)
+            zeroTotal++;
+    }
+
+    if (current.Value == 0)
+        zeroCount++;
+}
+
+Console.WriteLine($"Zero encountered {zeroCount} times at end of move.");
+Console.WriteLine($"Zero encountered a total of {zeroTotal} times.");
+
+static List<string> LoadInput(string fn)
+{
+    var appDir = AppDomain.CurrentDomain.BaseDirectory;
+    var filePath = Path.Combine(appDir, "Data", fn);
+
+    return File.ReadAllLines(filePath).ToList();
+}
+
+static DoublyLinkedListNode<int> PopuldateDial(DoublyLinkedList<int> dial)
+{
+    for (int i = 0; i <= 99; i++)
+    {
+        dial.AddLast(i);
+    }
+
+    dial.Head.Prev = dial.Tail;
+    dial.Tail.Next = dial.Head;
+
+    var current = dial.Head;
+    while (current.Value != 50)
+    {
+        current = current.Next;
+    }
+
+    return current;
+}
