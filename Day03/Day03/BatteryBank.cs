8510098a6dabc7112e7f3a518457307d691f587c
R. Scott Webber <swebber@thehwpgroup.com>
2025-12-07 09:54:21 -0500

    day 3 wip

---
+++ Day03/Day03/BatteryBank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day03;
+
+internal class BatteryBank
+{
+    private string _sequence = string.Empty;
+
+    public BatteryBank(string sequence)
+    {
+        _sequence = sequence;
+    }
+
+    public int Joltage()
+    {
+        int result = MaxLeadingDigitWithRemainder();
+        Console.WriteLine($"Sequence {_sequence}, Joltage {result}");
+        return result;
+    }
+
+    private int MaxDigit(string sequence)
+    {
+        int max = 0;
+        foreach (char c in sequence)
+        {
+            int digit = c - '0';
+            if (digit > max)
+                max = digit;
+        }
+        return max;
+    }
+
+    private int MaxLeadingDigitWithRemainder()
+    {
+        int max = 0;
+        int maxIndex = -1;
+        var leading = _sequence[..^1];
+        for (int i = 0; i < leading.Length; i++)
+        {
+            int digit = leading[i] - '0';
+            if (digit > max)
+            {
+                max = digit;
+                maxIndex = i;
+            }
+        }
+
+        string beyond = (maxIndex >= 0 && maxIndex + 1 < _sequence.Length) ? _sequence[(maxIndex + 1)..] : string.Empty;
+        var beyondMaxDigit = MaxDigit(beyond);
+
+        int result = (max * 10) + beyondMaxDigit;
+
+        return result;
+    }
+}
---
+++ Day03/Day03/Program.cs
@@ -0,0 +1,27 @@
+﻿using Day03;
+
+List<BatteryBank> input = ReadInput("data01.txt");
+Console.WriteLine($"Read {input.Count} battery banks from input file.");
+
+long total = 0;
+foreach (var bank in input)
+{
+    total += bank.Joltage();
+}
+
+Console.WriteLine($"Total joltage: {total}");
+
+static List<BatteryBank> ReadInput(string fn)
+{
+    var result = new List<BatteryBank>();
+
+    var appDir = AppContext.BaseDirectory;
+    var filePath = Path.Combine(appDir, "Data", fn);
+    
+    foreach (var line in File.ReadLines(filePath))
+    {
+        result.Add(new BatteryBank(line));
+    }
+
+    return result;
+}
